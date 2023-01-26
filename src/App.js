ebc20ef4b54758fbfe805ac3d61bdf72a2cb6d93
¨mago¨ <martingramajo08@gmail.com>
2023-01-26 13:55:06 -0300

    skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/Banner';
 import NavReact from './components/NavReact';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div >
       <NavReact />
       <Banner />
+      <Skills />
     </div>
   );
 }
