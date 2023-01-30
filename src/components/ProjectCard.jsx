db186c6bc2056cbeefaf113a2c515f1014df5b9f
¨mago¨ <martingramajo08@gmail.com>
2023-01-30 00:23:08 -0300

    agregar projectcard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,7 +11,12 @@ export default function ProjectCard({
 }) {
   return (
     <Col size={12} sm={6} md={4}>
-      <a className="text-decoration-none text-white" href={url}>
+      <a
+        className="text-decoration-none text-white"
+        href={url}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div className="proj-imgbx">
           <img src={imgUrl} />
           <div className="proj-txtx">
